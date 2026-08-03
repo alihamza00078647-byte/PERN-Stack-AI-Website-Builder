cb0c15f02d8bd100982703d05ed2db0cd562124b
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-08-03 12:47:06 +0500

    Fixing path bug

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,25 @@
 import "dotenv/config";
 import express, { type Request, type Response } from 'express';
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
 // Allow URLS Cors Options
 const corsOption = {
     origin: process.env.TRUSTED_ORIGIN?.split(',') || [],
-    credential: true
+    credential: true,
 }
 
 
 // Middleware
 app.use(cors(corsOption));
 app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+
 
 const port = process.env.PORT || 3000;
 
